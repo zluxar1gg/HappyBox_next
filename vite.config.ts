dd0ebfc813aee90c89bb3fc5e2bed7065b5ae938
zluxar1gg <dima.knyazev0@gmail.com>
2025-12-29 11:31:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,1 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    // Эта строка предотвращает ошибку "process is not defined" (Белый экран)
-    'process.env': {}
-  }
-});
